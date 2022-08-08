94287f99a724e99d9b2b20456d1d49f3cce1ea17
Megalapka <forvsykoe@list.ru>
2022-08-08 19:43:08 +0300

    push with passed test

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -19,7 +19,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("angn"))
+                .body("data", equalTo("ann"))
                 ;
 
     }
